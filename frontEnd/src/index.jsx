e33b70af5ebc3faf472228215649f27aa1923aa4
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-10-11 12:35:32 -0400

    Updated index.jsx

--- frontEnd/src/index.jsx
+++ frontEnd/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./App.css";
 import App from "./App";
 //IMPORT BrowserRouter and rename it to Router
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 //Wrap the App Component with the Router component to enable the router features
 const root = createRoot(document.getElementById("root")); // Replace 'root' with your root element ID
